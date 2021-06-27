09a4f5bb35f91a608061f5fc4bae5f5b36256519
Eric Ahnell <eric.ahnell@puttysoftware.com>
2021-06-27 14:18:55 -0400

    Format code.

--- lib-polynomial-table/src/module-info.java
+++ lib-polynomial-table/src/module-info.java
@@ -1,5 +1,5 @@
 module com.puttysoftware.polytable {
-	requires transitive com.puttysoftware.xio;
-	
-	exports com.puttysoftware.polytable;
+    requires transitive com.puttysoftware.xio;
+
+    exports com.puttysoftware.polytable;
 }
